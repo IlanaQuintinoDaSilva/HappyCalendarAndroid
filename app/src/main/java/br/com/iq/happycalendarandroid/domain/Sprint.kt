efc07753f59b9bb460ddc43cd67aec7a9adb8a3f
Ilana Quintino da Silva <ilana_silva@hotmail.com>
2018-09-15 22:07:09 -0300

    todoList: addition of more models.

---
+++ app/src/main/java/br/com/iq/happycalendarandroid/domain/Sprint.kt
@@ -0,0 +1,9 @@
+package br.com.iq.happycalendarandroid.domain
+
+import java.util.*
+
+class Sprint {
+    lateinit var startDate: Date
+    lateinit var endDate: Date
+    var sprintName: String = ""
+}
--- app/src/main/java/br/com/iq/happycalendarandroid/domain/ToDo.kt
+++ app/src/main/java/br/com/iq/happycalendarandroid/domain/ToDo.kt
@@ -2,5 +2,5 @@ package br.com.iq.happycalendarandroid.domain
 
 class ToDo{
     var id: Long = 0
-    var toDo: String = ""
+    var description: String = ""
 }
