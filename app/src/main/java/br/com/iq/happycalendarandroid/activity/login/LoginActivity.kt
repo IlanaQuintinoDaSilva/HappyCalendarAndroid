30db00a359b77b705886c1994a8265e5636411ad
Ilana Quintino da Silva <ilanaquintinodasilva@gmail.com>
2018-10-14 16:21:30 -0300

    Login screen.

---
+++ app/src/main/java/br/com/iq/happycalendarandroid/activity/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.iq.happycalendarandroid.activity.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.iq.happycalendarandroid.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/br/com/iq/happycalendarandroid/domain/api/GetProjectsService.kt
+++ app/src/main/java/br/com/iq/happycalendarandroid/domain/api/GetProjectsService.kt
@@ -5,11 +5,13 @@ import com.google.gson.JsonObject
 
 object GetProjectsService {
     fun getProjects():ArrayList<ProjectVO>{
-        val url = "http://localhost:49792/api/happycalendar/GetProjects"
+        var temp:  ArrayList<ProjectVO> = ArrayList()
+        /*val url = "http://localhost:49792/api/happycalendar/GetProjects"
 
         val obj = JsonObject()
         val request = JsonObject()
         val jsonRequest = request.toJson()
-        val httpHelper = getHttpHelper()
+        val httpHelper = getHttpHelper()*/
+        return temp
     }
 }
