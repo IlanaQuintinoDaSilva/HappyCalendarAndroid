b1fb0a287bd4bb63f4e1579bf3408d1b7d022726
Ilana Quintino da Silva <ilanaquintinodasilva@gmail.com>
2019-01-26 10:19:08 -0200

    nav drawer, first part, done

--- app/src/main/java/br/com/iq/happycalendarandroid/activity/ToDoListActivity.kt
+++ app/src/main/java/br/com/iq/happycalendarandroid/activity/ToDoListActivity.kt
@@ -2,6 +2,8 @@ package br.com.iq.happycalendarandroid.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import br.com.iq.happycalendarandroid.R
 import br.com.iq.happycalendarandroid.domain.Category
 import br.com.iq.happycalendarandroid.extensions.addFragment
@@ -10,14 +12,29 @@ import br.com.iq.happycalendarandroid.fragment.ToDoListFragment
 
 class ToDoListActivity : BaseActivity() {
 
+    private lateinit var mDrawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_list)
 
-        val category = intent.getSerializableExtra("category") as Category
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
         val title = getString(R.string.tarefas)
 
-        setupToolbar(R.id.toolbar, title, true)
+        setupToolbar(R.id.toolbar, title, false)
 
         if(savedInstanceState == null){
             addFragment(R.id.container, ToDoListFragment())
